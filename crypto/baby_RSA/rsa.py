e103b84a888e0462d50faf11386bfef5489c482e
Andre Hu <andrebhu@gmail.com>
2020-09-18 10:37:50 -0400

    fix solve scripts

--- crypto/baby_RSA/rsa.py
+++ crypto/baby_RSA/rsa.py
@@ -1,19 +1,11 @@
 from Crypto.Util.number import bytes_to_long, getPrime, inverse, long_to_bytes
 
-flag = "flag{really_s1ck_algor1thm}"
-p = getPrime(1024)
-q = getPrime(1024)
-e = 65537
-m = bytes_to_long(flag.encode("utf-8"))
-
+f = open("output.txt", "r")
+p = int(f.readline()[2:])
+q = int(f.readline()[2:])
+e = int(f.readline()[2:])
+c = int(f.readline()[2:])
 n = p * q
-c = pow(m, e, n)
-
-print("p:" + str(p))
-print("q:" + str(q))
-print("e:" + str(e))
-print("c:" + str(c))
-
 phi = (p - 1) * (q - 1)
 d = inverse(e, phi)
 print(long_to_bytes(pow(c, d, n)))
