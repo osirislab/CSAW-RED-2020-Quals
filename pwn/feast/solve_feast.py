425f7cb027a5dbc7ccfcead2937cc3926b08178e
jangui <jaimitino@gmail.com>
2020-09-18 13:19:10 -0400

    wrong hostname in solver

--- pwn/feast/solve_feast.py
+++ pwn/feast/solve_feast.py
@@ -2,7 +2,7 @@
 
 from pwn import *
 
-local = True
+local = False
 debug = False
 if local:
     if debug:
@@ -15,7 +15,7 @@ if local:
         #p = process('./feast')
         p = remote('localhost', 5000)
 else:
-    p = remote('pwn.chal.csaw.io', 5001)
+    p = remote('pwn.red.csaw.io', 5001)
 
 e = ELF('./feast')
 #print("win is at " + hex(e.sym['winner_winner_chicken_dinner']))
