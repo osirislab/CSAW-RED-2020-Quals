d935f93ef98f187cf26de8c3f78286c2b3ffe7ae
WvonStuermer <WVonStuermer@newmusicusa.org>
2020-09-18 02:18:45 -0400

    typo corrections...

--- rev/LanguageIsSimplyPerfect/f.lisp
+++ rev/LanguageIsSimplyPerfect/f.lisp
@@ -3,6 +3,6 @@
 
 (defun &(~)(if(< ~ |)~(+ (&(- ~ 1))(&(- ~ |)))))(&(*(let((% |)(@ $))(* % @))(let((^ |)(! $))(+ ^ !))))
 
-; Apparently during the read-out of the tape two numbers were replaced with the non-executing characters '|'' and '$'.
+; Apparently during the read-out of the tape two numbers were exchanged with the non-executing characters '|'' and '$'.
 ; Which integers do these two need to become to make the code return the value 832040?"
 ; Write the answer as flag{a,b} where “a” is the integer representing '|' and “b” is the integer representing '$'.
